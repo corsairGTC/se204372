f4ae873c2241a30eaec99a22cb97c72bcd6c712f
Narawutbr <78654022+Narawutbr@users.noreply.github.com>
2021-02-08 13:21:57 +0700

    Update OurGroup.java

--- OurGroup.java
+++ OurGroup.java
@@ -9,6 +9,7 @@ public class OurGroup {
         groupMembers.add("Teerawat Chanu 6120501711 "); //captain
         groupMembers.add("Kongphop Assawarerkpirom 6120502653"); //PUT YOUR NAME HERE
         groupMembers.add("Kanin Wongsuree 6120502661"); //PUT YOUR NAME HERE
+        groupMembers.add("Narawut Boonruang 6120503901 "); //PUT YOUR NAME HERE
         groupMembers.add(" "); //PUT YOUR NAME HERE
     }
     public Vector<String> getGroupMembers () {
