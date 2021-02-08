677763df4bcbe74f49d63fd7e486457abdaba892
ธีรวัฒน์ <78473096+corsairGTC@users.noreply.github.com>
2021-02-08 13:18:59 +0700

    Update OurGroup.java

--- OurGroup.java
+++ OurGroup.java
@@ -6,6 +6,7 @@ public class OurGroup {
     public OurGroup()
     {
         groupMembers = new Vector<String>();
+        groupMembers.add("Teerawat Chanu 6120501711 "); //captain
         groupMembers.add("Kongphop Assawarerkpirom 6120502653"); //PUT YOUR NAME HERE
         groupMembers.add("Kanin Wongsuree 6120502661"); //PUT YOUR NAME HERE
         groupMembers.add(" "); //PUT YOUR NAME HERE
